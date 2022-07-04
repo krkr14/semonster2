6bfc92297828188222412fd810e7572ef0306530
krkr14 <e1c19039@st.oit.ac.jp>
2022-07-04 15:44:12 +0900

    Monsterクラスのevolveメソッドのテスト作成

--- app/src/test/java/semonster2/MonsterTest.java
+++ app/src/test/java/semonster2/MonsterTest.java
@@ -6,7 +6,10 @@ import org.junit.Test;
 public class MonsterTest {
   @Test
   public void testSummonMonster1() {
-    Monster monster = new Monster(0, 0);
-    Assert.assertEquals("スライム:レア度[0]\n", monster.toString());
+    Monster monster1 = new Monster(0, 0);
+    Assert.assertEquals("スライム:レア度[0]\n", monster1.toString());
+    Monster monster2 = new Monster(0, 5);
+    monster2.evolve();
+    Assert.assertEquals("超スライム:レア度[5]\n", monster2.toString());
   }
 }
